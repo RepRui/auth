41655e2b220acd8f25dd7f1906ea7cfa43d75232
lishihai <dslsh@dscom>
2024-07-19 17:32:01 +0800

    commit

--- src/main/java/com/li/auth/dto/RequestUsername.java
+++ src/main/java/com/li/auth/dto/RequestUsername.java
@@ -10,16 +10,16 @@ public class RequestUsername implements Serializable {
     @Serial
     private static final long serialVersionUID = 1L;
 
-    @NotBlank(message = "id不能为空")
+    @NotBlank(message = "用户名不能为空")
     @Schema(description = "主键ID",requiredMode = Schema.RequiredMode.REQUIRED)
     private String username;
 
 
-    public @NotBlank(message = "id不能为空") String getUsername() {
+    public @NotBlank(message = "用户名不能为空") String getUsername() {
         return username;
     }
 
-    public void setUsername(@NotBlank(message = "id不能为空") String username) {
+    public void setUsername(@NotBlank(message = "用户名不能为空") String username) {
         this.username = username;
     }
 }
