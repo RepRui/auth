abe8fef5c8d9bc4522f88aadc8d7b7df5aa6ee54
元离 <66455588+RepRui@users.noreply.github.com>
2024-08-05 14:51:38 +0800

    Update AuthController.java

--- src/main/java/com/li/auth/controller/AuthController.java
+++ src/main/java/com/li/auth/controller/AuthController.java
@@ -15,6 +15,7 @@ import jakarta.validation.Valid;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.web.bind.annotation.*;
 
 @Tag(name = "登录注销")
